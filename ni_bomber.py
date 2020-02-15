a819a48bf416bba66c45bf0e36d21798b25c9e4c
Nikait <50284221+Nikait@users.noreply.github.com>
2020-02-15 22:08:23 +0300

    Update ni_bomber.py

--- ni_bomber.py
+++ ni_bomber.py
@@ -2,39 +2,53 @@ import requests
 import datetime
 import services
 
+#colours
 class color:
 	Green = '\033[92m'
 	Cyan = '\033[95m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'
+	Red = '\033[91m'
 
-print(color.Green + color.BOLD + '     ' + color.UNDERLINE + '[NI BOMBER 2.0]' + color.END)
-print(color.Cyan + color.BOLD + "-"*25 + color.END)
-print(color.Cyan + color.BOLD + '| coded by  | nikait    |\n' + '| telegram: | @aaanikit |' + color.END)
-print(color.Cyan + color.BOLD + "-"*25 + color.END)
-print('')
+#header
+print(color.Green + color.BOLD + '         ' + color.UNDERLINE + '[NI BOMBER 2.3]' + color.END)
 
+print()
+print(color.BOLD + "coded by" + color.END, end = "")
+print(color.Green + color.BOLD + " >> " + color.END, end = "")
+print(color.Cyan + color.BOLD + "nikait" + color.END)
 
-print('введите номер без префиксов\nпример: 9018017010')
+print(color.BOLD +  "telegram" + color.END, end = "")
+print(color.Green + color.BOLD + " >> " + color.END, end = "")
+print(color.Cyan + color.BOLD + "@aaanikit" + color.END)
+print()
+
+#inputs
+print('enter the number without prefixes\nexample: 9018017010')
 number = input(color.Green + color.BOLD + '>> ' + color.END)
-print('сколько sms отправить?')
+print('how many sms to send?')
 sms = int(input(color.Green + color.BOLD + '>> ' + color.END))
+
+print("you need a" + color.Cyan + " tor " + color.END + "y/n? ")
+is_tor = input(color.BOLD + color.Green + ">> " + color.END)
+
 
 def check_number(number):
 	if int(len(number)) == 10 and int(number[0]) == 9:
 		print('[*]check number -' + color.Green + color.BOLD + ' OK' + color.END)
 	else:
-		print('[*]check number -' + color.Green + color.BOLD + ' неправильный номер' + color.END)
+		print('[*]check number -' + color.Red + color.BOLD + ' failed number!' + color.END)
 		quit()
 check_number(number)
 
-is_tor = input("нужен tor y/n ? ")
+
+#tor
 if str(is_tor) == "y":
-	print('[*]запуск ' + color.Cyan + color.BOLD + 'Tor' + color.END + '...')
-	proxies = {'http': 'socks5://139.59.53.105:1080','https': 'socks5://139.59.53.105:1080'}
-	tor = requests.get('http://icanhazip.com/', proxies=proxies).text
-	tor = (tor.replace('\n',''))
-	print('[*]запуск ' + color.Cyan + color.BOLD + 'Tor' + color.END + ' - ' +  color.Green + color.BOLD + 'OK ваше очко в безопасности' + color.END)
+        print('[*]launch ' + color.Cyan + color.BOLD + 'Tor' + color.END + '...')
+        proxies = {'http': 'socks5://139.59.53.105:1080','https': 'socks5://139.59.53.105:1080'}
+        tor = requests.get('http://icanhazip.com/', proxies=proxies).text
+        tor = (tor.replace('\n',''))
+        print('[*]launch ' + color.Cyan + color.BOLD + 'Tor' + color.END + ' - ' +  color.Green + color.BOLD + 'OK' + color.END)
 
 services.attack(number, sms)
